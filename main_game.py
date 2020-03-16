9dd469bac2ef814317d4d94e87ac06c687d75fe7
P G <p_godl01@uni-muenster.de>
2020-03-16 16:47:04 +0100

    Lanes added

--- main_game.py
+++ main_game.py
@@ -19,21 +19,26 @@ myfont = pygame.font.SysFont('Comic Sans MS', FONT_SIZE)
 game_live = True
 
 # Test Area
-myRect = pygame.Rect(0,100,50,50)
-stepw = 10
+nolanes = 10
+stepw = SCREEN_WIDTH/(nolanes)
+carheight=SCREEN_HEIGHT/10
+myRect = pygame.Rect(0,SCREEN_HEIGHT-carheight,SCREEN_WIDTH/nolanes,carheight)
+
 
 while game_live:
     time.sleep(0.01)
     screen.fill(mycolors.white)
+    for lane in range(1,nolanes):
+        pygame.draw.line(screen, mycolors.black,(SCREEN_WIDTH/nolanes*lane,0),(SCREEN_WIDTH/nolanes*lane,SCREEN_HEIGHT),1)
     # Check for events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_live = False
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
-                myRect.y -= stepw
-            if event.key == pygame.K_DOWN:
-                myRect.y += stepw
+            #if event.key == pygame.K_UP:
+            #    myRect.y -= stepw
+            #if event.key == pygame.K_DOWN:
+            #    myRect.y += stepw
             if event.key == pygame.K_LEFT:
                 myRect.x -= stepw
             if event.key == pygame.K_RIGHT:
