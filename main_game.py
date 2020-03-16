cc759fcd3cab740c8d779f09b9947af92425c494
Sven <svenupgang@gmx.de>
2020-03-16 12:15:31 +0100

    green rectangle

--- main_game.py
+++ main_game.py
@@ -5,14 +5,19 @@ import mycolors
 pygame.init()
 pygame.font.init()
 
+SCREEN_HEIGHT = 800
+SCREEN_WIDTH = 600
+FONT_SIZE = 20
+
 # Create the screen
-screen = pygame.display.set_mode((800, 600))
+screen = pygame.display.set_mode((SCREEN_HEIGHT, SCREEN_WIDTH))
 pygame.display.set_caption('Python Project')
-myfont = pygame.font.SysFont('Comic Sans MS', 20)
+myfont = pygame.font.SysFont('Comic Sans MS', FONT_SIZE)
 
 # globale game variables
 game_live = True
 
+# Test Area
 myRect = pygame.Rect(100, 100, 50, 50)
 
 while game_live:
