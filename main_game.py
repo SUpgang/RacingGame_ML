4445a742cc6763497da48f5e3a41c107b8f7d6d2
P G <p_godl01@uni-muenster.de>
2020-03-16 14:10:59 +0100

    Movement possible

--- main_game.py
+++ main_game.py
@@ -1,16 +1,17 @@
 import pygame
 import mycolors
+import time
 
 # Initialize pygame
 pygame.init()
 pygame.font.init()
 
-SCREEN_HEIGHT = 800
-SCREEN_WIDTH = 600
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
 FONT_SIZE = 20
 
 # Create the screen
-screen = pygame.display.set_mode((SCREEN_HEIGHT, SCREEN_WIDTH))
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('Python Project')
 myfont = pygame.font.SysFont('Comic Sans MS', FONT_SIZE)
 
@@ -18,18 +19,33 @@ myfont = pygame.font.SysFont('Comic Sans MS', FONT_SIZE)
 game_live = True
 
 # Test Area
-myRect = pygame.Rect(200, 100, 50, 50)
+myRect = pygame.Rect(0,100,50,50)
+stepw = 10
 
 while game_live:
-
+    time.sleep(0.01)
+    screen.fill(mycolors.white)
     # Check for events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_live = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                myRect.y -= stepw
+            if event.key == pygame.K_DOWN:
+                myRect.y += stepw
+            if event.key == pygame.K_LEFT:
+                myRect.x -= stepw
+            if event.key == pygame.K_RIGHT:
+                myRect.x += stepw
+
 
     # Draw things
     pygame.draw.rect(screen, mycolors.green, myRect)
     pygame.display.flip()
+
+
+
 
 # Quit game after loop
 pygame.quit()
