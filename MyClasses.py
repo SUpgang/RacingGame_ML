c716f76c38d54fbf5ee7ef5f465cd02747eab41a
Sven <svenupgang@gmx.de>
2020-03-23 11:23:55 +0100

    Started Restructuring

--- MyClasses.py
+++ MyClasses.py
@@ -1,4 +1,55 @@
 import pygame
+import numpy as np
+
+class Street:
+    '''The street and its movement
+
+        Attributes:
+            number_of_lanes: integer
+            image_lane: pygame image for one lane
+            speed = (0, speed_y): numpy array
+            height: equals height of one lane
+            width: equals width_of_lane * number_of_lanes
+            agents: list of agents on the street
+
+    '''
+
+    def __init__(self):
+        pass
+
+
+class TrafficAgents:
+    '''An object on the street (including the player, enemies and fixed obstacles)
+
+        Attributes:
+            position = (pos_x, pos_y): numpy array
+            lane: current lane, integer, 1-to-1 with position
+            image: pygame image
+            collision_rect: pygame rect to check for collisions
+            speed = (speed_x, speed_y): numpy array, number of pixel to move every tick
+
+    '''
+
+    _number_of_agents = 0
+
+    def __init__(self):
+
+        TrafficAgents._number_of_agents += 1
+
+class GameSession:
+    '''The Session which handles one game including Street and Agents
+
+        Attributes:
+            street: Street-object
+            player:
+            screen:
+            enemies_list: List of all enemies
+
+
+        '''
+
+    def __init__(self):
+        pass
 
 class Enemy:
     '''Enemies which occur on the lanes'''
