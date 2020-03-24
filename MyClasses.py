ac07d0a6f3abe2f55cb6fb92030a34948ea771ec
Sven <svenupgang@gmx.de>
2020-03-24 18:53:18 +0100

    Cleaned Version of finished structure

--- MyClasses.py
+++ MyClasses.py
@@ -2,29 +2,29 @@ import pygame
 import numpy as np
 import mycolors
 
+
 class TrafficAgents:
     """An object on the street (including the player, enemies and fixed obstacles)
 
         Attributes:
-            agent_type: 'enemy' for enemy car, 'player' for player
             lane: current lane, integer, 1-to-1 with position
-            position = (pos_x, pos_y): numpy array
-            speed = (speed_x, speed_y): numpy array, number of pixel to move every tick
-            image: pygame image
-            image_width: width of image in pixels
-            image_height: width of image in pixels
-            collision_rect: pygame rect to check for collisions
             _lane_width: lane width in pixels
             _screen_height: screen height in pixels
             _screen_width: screen width in pixels
+            agent_type: 'enemy' for enemy car, 'player' for player
+            image: pygame image
+            position = (pos_x, pos_y): numpy array
+            speed = (speed_x, speed_y): numpy array, number of pixel to move every tick
+            collision_rect: pygame rect to check for collisions
 
         Methods:
-            update_position(self)
+            get_pos_x_from_lane(self): returns the pos_x according to self.lane
+            get_lane_from_position(self): returns lane calculated from the position[0]
+            update_position(self, manual_speed)
             check_collision(self, other_rect)
-            get_pos_x(self): returns the pos_x according to self.lane
+            draw(self)
 
     """
-    _number_of_agents = 0
 
     def __init__(self, screen_height, screen_width, lane_width, starting_lane=1, agent_type='enemy'):
         self.lane = starting_lane
@@ -34,45 +34,42 @@ class TrafficAgents:
         self.agent_type = agent_type
         if agent_type == 'player':
             self.image = pygame.image.load('car_sprite.png')
-            self.image_height = self.image.get_height()
-            pos_y = screen_height-self.image_height
+            pos_y = screen_height-self.image.get_height()
             speed_x = 0
             speed_y = 0
         else:
             self.image = pygame.image.load('car_sprite_enemy.png')
-            self.image_height = self.image.get_height()
-            pos_y = -self.image_height
+            pos_y = -self.image.get_height()
             speed_x = 0
             speed_y = 2
-        self.image_width = self.image.get_width()
-        self.position = np.array([self.get_pos_x(),pos_y])
+        self.position = np.array([self.get_pos_x_from_lane(),pos_y])
         self.speed = np.array([speed_x,speed_y])
-        self.collision_rect = pygame.Rect((self.position[0], self.position[1]), (self.image_width, self.image_height))
+        self.collision_rect = pygame.Rect((self.position[0], self.position[1]), (self.image.get_width(),
+                                                                                 self.image.get_height()))
 
-        TrafficAgents._number_of_agents += 1
+    def get_pos_x_from_lane(self):
+        return self._lane_width * (self.lane - 1) + (self._lane_width - self.image.get_width()) / 2
 
-    def get_pos_x(self):
-        return self._lane_width * (self.lane - 1) + (self._lane_width - self.image_width) / 2
+    def get_lane_from_position(self):
+        return int(round((self.position[0] - (self._lane_width - self.image.get_width()) / 2 ) / self._lane_width, 1)+1)
 
     def update_position(self, manual_player_speed):
         if self.agent_type == 'player':
             self.speed = manual_player_speed
-        self.position += self.speed
 
-        #undo update if player leaves the screen
-        if (self.agent_type == 'player') and ((self.position[0] < 0) or (self.position[0] > self._screen_width)):
-            self.position -= self.speed
-        else:
-            #move collision_rect only if player does not leave screen
+        new_position = self.position + self.speed
+
+        # integrity check for new position
+        if new_position[0] >= 0 and new_position[0] + self.image.get_width() <= self._screen_width:
+            self.position = new_position
             self.collision_rect.move_ip(self.speed[0], self.speed[1])
+            self.lane = self.get_lane_from_position()
 
-        if self.agent_type == 'player':
-            return True
-        elif self.position[1] > self._screen_height:
+        # check if agent cant be deleted
+        if new_position[1] > self._screen_height:
             return False
         else:
             return True
-
 
     def check_collision(self, other_rect):
         return self.collision_rect.colliderect(other_rect.collision_rect)
@@ -85,27 +82,34 @@ class GameSession:
     """ Underlying object which manages all parts of a GameSession
 
         Attributes:
-            req_screen_height: requiered screen height
-            req_screen_width: requiered screen width
-            FPS: 60
             game_clock: clock for the game to count the loops
+            FPS: 60
+            screen
 
             lane_image = 'street_sprite.png'
             lane_width = 100: given by pixels of picture
             lane_height = 500: given by pixels of picture
 
             number_of_lanes: integer
-
-            sprite_position = (pos_x, pos_y): numpy array, starting value to draw lanes
-            speed = (0, speed_y): numpy array
-
             agent_list: agent[0] is always the player
+            man_player_speed: to handle inputs and move the car
+
+            t: number of ticks of the games
+
+            spawning_probability: prob to spawn a new enemy, increasing with time
+
+            req_screen_height: requiered screen height
+            req_screen_width: requiered screen width
 
             live = True: status of the game, running or not
 
         Methods:
-            tick()
-            draw_street(self)
+            tick(self)
+            handle_events(self)
+            update_positions(self)
+            draw(self)
+            gameloop(self)
+
     """
 
     def __init__(self, player_type = 'manual', number_of_lanes=8, lane_sprite='street_sprite.png', fps=60, screen=[]):
@@ -134,29 +138,35 @@ class GameSession:
         if not screen == []:
             self.screen = pygame.display.set_mode((self.req_screen_width, self.req_screen_height))
 
-        self.agent_list.append(TrafficAgents(agent_type='player', screen_height=self.req_screen_height, lane_width=self.lane_width))
+        self.agent_list.append(TrafficAgents(screen_height=self.req_screen_height, screen_width=self.req_screen_width,
+                                             lane_width=self.lane_width, starting_lane=3, agent_type='player'))
         self.live = True
 
     def tick(self):
         self.game_clock.tick(self.FPS)
         self.t += 1
 
-    def spawn_enemies(self):
+    def spawn_new_enemy(self):
         if np.random.rand() < self.spawning_probability:
-            starting_lane = np.random.randint(1, self.number_of_lanes+1)
-            self.agent_list.append(TrafficAgents(lane_width=self.lane_width, screen_height=self.req_screen_height, starting_lane=starting_lane))
+            spawn_lane = np.random.randint(1, self.number_of_lanes+1)
 
-            # spawning_collision = False
-            # for enemy in enemy_list:
-            #     if new_enemy.collision_rect.colliderect(enemy.collision_rect):
-            #         spawning_collision = True
-            #         # print('Did not spawn at lane {}'.format(starting_lane))
-            #
-            # if not spawning_collision:
-            #     enemy_list.append(new_enemy)
-            # p = 0
+            new_agent = TrafficAgents(screen_height=self.req_screen_height, screen_width=self.req_screen_width,
+                                      lane_width=self.lane_width, starting_lane=spawn_lane)
+
+            agents_at_same_lane_list = self.get_list_of_agents_at_lane(new_agent.lane)
+
+            if new_agent.collision_rect.collidelist(agents_at_same_lane_list) == -1:
+                self.agent_list.append(new_agent)
+                self.spawning_probability = 0
+
         else:
-            self.spawning_probability += round(1 / ((len(self.agent_list)) * self.FPS * 50), 6)
+            self.spawning_probability += round(1 / ((len(self.agent_list)) * self.FPS * 50), 8)
+
+    def get_list_of_agents_at_lane(self, lane):
+        if len(self.agent_list) > 0:
+            return [agent.collision_rect for agent in self.agent_list if agent.lane == lane]
+        else:
+            return []
 
     def handle_events(self):
         """For events like quit or userinputs"""
@@ -175,6 +185,14 @@ class GameSession:
                 self.agent_list.pop(i)
             elif i == 0:
                 self.man_player_speed = np.array([0,0])
+
+    def check_collisions_with_player(self):
+        enemies_at_lane = self.get_list_of_agents_at_lane(self.agent_list[0].lane)
+        enemies_at_lane.pop(0)
+        if not self.agent_list[0].collision_rect.collidelist(enemies_at_lane) == -1:
+            return True
+        else:
+            return False
 
     def draw(self, street_speed=1):
         """Draws the street to screen according to the current speed"""
@@ -196,34 +214,9 @@ class GameSession:
     def gameloop(self):
         """All functions needed for one loop cycle"""
         self.tick()
-        self.spawn_enemies()
+        self.spawn_new_enemy()
         self.handle_events()
         self.update_positions()
         self.draw()
-
-
-
-class Enemy:
-    """Enemies which occur on the lanes"""
-
-    def __init__(self, starting_lane = 1, image='car_sprite_enemy.png', street_width = 100, speed_delta_px_per_tick = 1):
-
-        self.lane = starting_lane
-        self._image_filename = image
-        self._street_width = street_width
-        self._delta_px_per_tick = speed_delta_px_per_tick
-
-        if image != '':
-            self.image = pygame.image.load(image)
-            self.image_width = 60
-            self.image_height = 100
-            self.pos_y_puffer_px = self.image_height
-
-        self.pos_x = 20 + (self.lane-1) * self._street_width
-        self.pos_y = -self.pos_y_puffer_px
-        self.collision_rect = pygame.Rect((self.pos_x, self.pos_y), (60, 100))
-
-    def update_position(self):
-        self.pos_y = self.pos_y + self._delta_px_per_tick
-        self.collision_rect.move_ip(0, self._delta_px_per_tick)
-
+        if self.check_collisions_with_player():
+            self.live = False
--- main_game.py
+++ main_game.py
@@ -5,6 +5,7 @@ from MyClasses import *
 pygame.init()
 pygame.font.init()
 my_screen = pygame.display.set_mode((800, 500))
+
 ## init pygame font
 # font_size = 20
 # myfont = pygame.font.SysFont('Comic Sans MS', font_size)
@@ -13,90 +14,8 @@ my_game_session = GameSession(screen=my_screen)
 
 while my_game_session.live:
 
-    #player = TrafficAgents(my_game_session,starting_lane=1, agent_type='player')
-    #my_game_session.screen.blit(player.image, (player.pos_x, player.pos_y))
-
     my_game_session.gameloop()
     pygame.display.flip()
 
-pygame.quit()
-quit()
-
-# ALLES AB HIER WIRD IGNORIERT
-
-# Game settings
-number_of_lanes = 8
-car_starting_lane = 1
-car_current_lane = car_starting_lane
-tick = 0
-delta_px_per_tick = 2
-cycle = int(lane_height/delta_px_per_tick)
-p = 0
-enemy_list = []
-player_collision_rect = pygame.Rect((20, 400), (60, 100))
-player_alive = True
-
-while game_live:
-    myclock.tick(FPS)
-
-    # Delete previous content
-    screen.fill(mycolors.black)
-
-    # Fill up the streets:
-    for i in range(number_of_lanes):
-        shift_px_y = delta_px_per_tick*tick%cycle
-        screen.blit(lane_image, (lane_width*i, shift_px_y))
-        screen.blit(lane_image, (lane_width*i, -lane_height + shift_px_y))
-
-    player_pos_x, player_pos_y = lane_width*(car_current_lane-1) + 20, 400
-    screen.blit(car_image, (player_pos_x, player_pos_y))
-    player_collision_rect = pygame.Rect((player_pos_x, player_pos_y), (60, 100))
-    #pygame.draw.rect(screen, mycolors.black, player_collision_rect, 1)
-
-    # Create enemies
-    if random.random() < p:
-        starting_lane = random.randint(1, number_of_lanes)
-        new_enemy = MyClasses.Enemy(starting_lane=starting_lane)
-        #print('Try to spawn new car at lane {}'.format(starting_lane))
-
-        spawning_collision = False
-        for enemy in enemy_list:
-            if new_enemy.collision_rect.colliderect(enemy.collision_rect):
-                spawning_collision = True
-                #print('Did not spawn at lane {}'.format(starting_lane))
-
-        if not spawning_collision:
-            enemy_list.append(new_enemy)
-        p = 0
-    else:
-        p += round(1 / ((len(enemy_list)+1)*FPS*50),6)
-
-    for i, enemy in enumerate(enemy_list):
-        if player_alive:
-            enemy.update_position()
-        screen.blit(enemy.image, (enemy.pos_x, enemy.pos_y))
-        #pygame.draw.rect(screen, mycolors.black, enemy.collision_rect, 1)
-        if enemy.pos_y > SCREEN_HEIGHT:
-            enemy_list.pop(i)
-        if player_collision_rect.colliderect(enemy.collision_rect):
-            player_alive = False
-            #print("dead at tick {}".format(tick))
-
-    # Check for events
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            game_live = False
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                car_current_lane = max(1, car_current_lane - 1)
-            if event.key == pygame.K_RIGHT:
-                car_current_lane = min(number_of_lanes, car_current_lane + 1)
-
-    # Show drawings on screen
-    pygame.display.flip()
-    if player_alive:
-        tick += 1
-
-# Quit game after loop
 pygame.quit()
 quit()
